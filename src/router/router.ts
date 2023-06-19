cec40561b2e59fc60f3e32187749dcccd46fda78
Nguyen-Quang-Sang-Jizo <117348347+Nguyen-Quang-Sang-Jizo@users.noreply.github.com>
2023-06-20 05:01:54 +0700

    update

--- src/router/router.ts
+++ src/router/router.ts
@@ -4,8 +4,6 @@ import providerRouter from "./providerRouter";
 import adminRouter from "./adminRouter";
 import serviceProviderRouter from "./serviceProviderRouter";
 import otpRouter from "./otpRouter";
-import typeRouter from "./typeRouter";
-import serviceRouter from "./serviceRouter";
 const hasPermissionsAdmin = require('../middleware/CheckRoleAdmin')
 const hasPermissionsProvider = require('../middleware/CheckRoleProvider')
 const hasPermissionsUser = require('../middleware/checkRoleUser')
@@ -13,13 +11,17 @@ const passport = require('passport');
 const router = Router();
 
 router.use('/users', userRouter);
-router.use('/providers',providerRouter);
+router.use('/providers', passport.authenticate('jwt', { session: false, failWithError: true }),
+(req, res, next) => {
+    hasPermissionsProvider(req, res, next, );
+},providerRouter);
 router.use('/admin', passport.authenticate('jwt', { session: false, failWithError: true }),
     (req, res, next) => {
         hasPermissionsAdmin(req, res, next, );
     },adminRouter);
-router.use('/serviceProvider',serviceProviderRouter);
-router.use('/otp',otpRouter);
-router.use('/type',typeRouter);
-router.use('/services',serviceRouter);
+router.use('/services', passport.authenticate('jwt', { session: false, failWithError: true }),
+    (req, res, next) => {
+        hasPermissionsUser(req, res, next, );
+    },serviceProviderRouter)
+router.use('/otp',otpRouter)
 export default router
