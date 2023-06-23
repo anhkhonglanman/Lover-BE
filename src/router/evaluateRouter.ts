3d21f849aa23d520af96fc5bc3d1cb78cbe16800
Nguyen-Quang-Sang-Jizo <117348347+Nguyen-Quang-Sang-Jizo@users.noreply.github.com>
2023-06-23 08:51:08 +0700

    update

--- src/router/evaluateRouter.ts
+++ src/router/evaluateRouter.ts
@@ -1,5 +1,6 @@
 import {Router} from "express";
+import evaluateController from "../controllers/evaluateController";
 
 const evaluzteRouter = Router()
-evaluzteRouter.get('/:id')
+evaluzteRouter.post('/:id', evaluateController.addEvaluate)
 export default evaluzteRouter
